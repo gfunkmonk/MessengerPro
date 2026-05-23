95eb793e3a279a2edaa135ee0dac129b6ebcbb59
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-05-23 23:36:39 +0300

    Update TypingIndicatorReceivedHook.java

--- app/src/main/java/tn/amin/mpro2/hook/all/TypingIndicatorReceivedHook.java
+++ app/src/main/java/tn/amin/mpro2/hook/all/TypingIndicatorReceivedHook.java
@@ -5,6 +5,8 @@ import android.os.Parcelable;
 import java.util.Set;
 
 import de.robv.android.xposed.XC_MethodHook;
+
+import tn.amin.mpro2.debug.Logger;
 import tn.amin.mpro2.hook.BaseHook;
 import tn.amin.mpro2.hook.HookId;
 import tn.amin.mpro2.hook.helper.ContextHookHelper;
@@ -13,8 +15,9 @@ import tn.amin.mpro2.orca.wrapper.ThreadKeyWrapper;
 import tn.amin.mpro2.orca.wrapper.UserKeyWrapper;
 
 public class TypingIndicatorReceivedHook extends BaseHook {
-    private final static String ACTION_TYPING_INDICATOR = "com.facebook.presence.ACTION_OTHER_USER_TYPING_CHANGED";
-//    private final static String ACTION_MESSAGE_RECEIVED = "com.facebook.presence.ACTION_PUSH_RECEIVED";
+
+    private static final String ACTION_TYPING_INDICATOR =
+            "com.facebook.presence.ACTION_OTHER_USER_TYPING_CHANGED";
 
     @Override
     public HookId getId() {
@@ -22,52 +25,144 @@ public class TypingIndicatorReceivedHook extends BaseHook {
     }
 
     @Override
-    protected Set<XC_MethodHook.Unhook> injectInternal(OrcaGateway gateway) {
-        return ContextHookHelper.interceptBroadcast(ACTION_TYPING_INDICATOR, wrap(intent -> {
-            Long userKey = -1L, threadKey = -1L;
-            boolean isTyping = false;
+    protected Set<XC_MethodHook.Unhook> injectInternal(
+            OrcaGateway gateway
+    ) {
 
-            int state = intent.getIntExtra("extra_new_state", -1);
-            Parcelable userKeyObject = intent.getParcelableExtra("extra_user_key");
-            Parcelable threadKeyObject = intent.getParcelableExtra("extra_thread_key");
+        try {
 
-            UserKeyWrapper userKeyWrapper = new UserKeyWrapper(userKeyObject);
-            userKey = userKeyWrapper.getUserKeyLong();
+            return ContextHookHelper.interceptBroadcast(
 
-            if (threadKeyObject != null) {
-                ThreadKeyWrapper threadKeyWrapper = new ThreadKeyWrapper(threadKeyObject);
-                if ("GROUP".equals(threadKeyWrapper.getType())) {
-                    threadKey = threadKeyWrapper.getGroupThreadKey();
-                }
-            }
-            if (threadKey == -1L) {
-                threadKey = userKey;
-            }
+                    ACTION_TYPING_INDICATOR,
 
-            isTyping = state == 1;
+                    wrap(intent -> {
 
-            final Long finalUserKey = userKey;
-            final Long finalThreadKey = threadKey;
-            final boolean finalIsTyping = isTyping;
-            notifyListeners((listener) -> ((TypingIndicatorReceivedListener) listener).onTypingIndicatorReceived(finalUserKey, finalThreadKey, finalIsTyping));
+                        try {
 
-            return false;
-        }), this::wrapIgnoreWorking);
+                            if (intent == null)
+                                return false;
 
-//                   TODO use this for messages history
-//
-//                    Object messageObject = intent.getParcelableExtra("extra_message");
-//
-//                    Logger.info("Received a new message ! " + messageObject);
-//
-//                    if (messageObject != null) {
-//                        MessageWrapper message = new MessageWrapper(gateway, messageObject);
-//                        Logger.logObjectRecursive(message);
-//                    }
-//                }
+                            Long userKey = -1L;
+                            Long threadKey = -1L;
+
+                            boolean isTyping = false;
+
+                            int state =
+                                    intent.getIntExtra(
+                                            "extra_new_state",
+                                            -1
+                                    );
+
+                            Parcelable userKeyObject =
+                                    intent.getParcelableExtra(
+                                            "extra_user_key"
+                                    );
+
+                            Parcelable threadKeyObject =
+                                    intent.getParcelableExtra(
+                                            "extra_thread_key"
+                                    );
+
+                            if (userKeyObject != null) {
+
+                                try {
+
+                                    UserKeyWrapper userKeyWrapper =
+                                            new UserKeyWrapper(
+                                                    userKeyObject
+                                            );
+
+                                    userKey =
+                                            userKeyWrapper.getUserKeyLong();
+
+                                } catch (Throwable t) {
+
+                                    Logger.error(t);
+                                }
+                            }
+
+                            if (threadKeyObject != null) {
+
+                                try {
+
+                                    ThreadKeyWrapper threadKeyWrapper =
+                                            new ThreadKeyWrapper(
+                                                    threadKeyObject
+                                            );
+
+                                    if ("GROUP".equals(
+                                            threadKeyWrapper.getType()
+                                    )) {
+
+                                        threadKey =
+                                                threadKeyWrapper
+                                                        .getGroupThreadKey();
+                                    }
+
+                                } catch (Throwable t) {
+
+                                    Logger.error(t);
+                                }
+                            }
+
+                            if (threadKey == -1L) {
+                                threadKey = userKey;
+                            }
+
+                            isTyping = state == 1;
+
+                            final Long finalUserKey =
+                                    userKey;
+
+                            final Long finalThreadKey =
+                                    threadKey;
+
+                            final boolean finalIsTyping =
+                                    isTyping;
+
+                            notifyListeners(listener -> {
+
+                                try {
+
+                                    ((TypingIndicatorReceivedListener) listener)
+                                            .onTypingIndicatorReceived(
+                                                    finalUserKey,
+                                                    finalThreadKey,
+                                                    finalIsTyping
+                                            );
+
+                                } catch (Throwable listenerError) {
+
+                                    Logger.error(listenerError);
+                                }
+                            });
+
+                        } catch (Throwable t) {
+
+                            Logger.error(t);
+                        }
+
+                        return false;
+                    }),
+
+                    this::wrapIgnoreWorking
+
+            );
+
+        } catch (Throwable t) {
+
+            Logger.error(t);
+
+            return null;
+        }
     }
 
     public interface TypingIndicatorReceivedListener {
-        void onTypingIndicatorReceived(long userKey, long threadKey, boolean isTyping);
+
+        void onTypingIndicatorReceived(
+                long userKey,
+                long threadKey,
+                boolean isTyping
+        );
     }
 }
