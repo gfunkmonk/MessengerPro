485e9025024e1fff0b800c10573b6b38dd9b27f0
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-05-23 23:40:54 +0300

    Update ConversationEnterHook.java

--- app/src/main/java/tn/amin/mpro2/hook/all/ConversationEnterHook.java
+++ app/src/main/java/tn/amin/mpro2/hook/all/ConversationEnterHook.java
@@ -3,6 +3,8 @@ package tn.amin.mpro2.hook.all;
 import java.util.Set;
 
 import de.robv.android.xposed.XC_MethodHook;
+
+import tn.amin.mpro2.debug.Logger;
 import tn.amin.mpro2.hook.BaseHook;
 import tn.amin.mpro2.hook.HookId;
 import tn.amin.mpro2.hook.HookTime;
@@ -11,6 +13,7 @@ import tn.amin.mpro2.hook.unobfuscation.OrcaUnobfuscator;
 import tn.amin.mpro2.orca.OrcaGateway;
 
 public class ConversationEnterHook extends BaseHook {
+
     @Override
     public HookId getId() {
         return HookId.CONVERSATION_ENTER;
@@ -22,18 +25,116 @@ public class ConversationEnterHook extends BaseHook {
     }
 
     @Override
-    protected Set<XC_MethodHook.Unhook> injectInternal(OrcaGateway gateway) {
-        return OrcaHookHelper.hookFeature(gateway.unobfuscator.getAPICode(OrcaUnobfuscator.API_CONVERSATION_ENTER),
-                "O", "Orca", gateway.classLoader, wrap(new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        long threadKey = Long.parseLong((String) param.args[6]);
-                        notifyListeners((listener) -> ((ConversationEnterListener) listener).onConversationEnter(threadKey));
-                    }
-                }));
+    protected Set<XC_MethodHook.Unhook> injectInternal(
+            OrcaGateway gateway
+    ) {
+
+        try {
+
+            Integer apiCode =
+                    gateway.unobfuscator.getAPICode(
+                            OrcaUnobfuscator.API_CONVERSATION_ENTER
+                    );
+
+            if (apiCode == null) {
+
+                Logger.error(
+                        "Conversation enter API code is null"
+                );
+
+                return null;
+            }
+
+            return OrcaHookHelper.hookFeature(
+
+                    apiCode,
+
+                    "O",
+
+                    "Orca",
+
+                    gateway.classLoader,
+
+                    wrap(new XC_MethodHook() {
+
+                        @Override
+                        protected void beforeHookedMethod(
+                                MethodHookParam param
+                        ) {
+
+                            try {
+
+                                if (param.args == null)
+                                    return;
+
+                                if (param.args.length < 7) {
+
+                                    Logger.error(
+                                            "Unexpected conversation enter args length: "
+                                                    + param.args.length
+                                    );
+
+                                    return;
+                                }
+
+                                Object arg =
+                                        param.args[6];
+
+                                if (!(arg instanceof String))
+                                    return;
+
+                                long threadKey;
+
+                                try {
+
+                                    threadKey =
+                                            Long.parseLong(
+                                                    (String) arg
+                                            );
+
+                                } catch (Throwable parseError) {
+
+                                    Logger.error(parseError);
+
+                                    return;
+                                }
+
+                                final long finalThreadKey =
+                                        threadKey;
+
+                                notifyListeners(listener -> {
+
+                                    try {
+
+                                        ((ConversationEnterListener) listener)
+                                                .onConversationEnter(
+                                                        finalThreadKey
+                                                );
+
+                                    } catch (Throwable listenerError) {
+
+                                        Logger.error(listenerError);
+                                    }
+                                });
+
+                            } catch (Throwable t) {
+
+                                Logger.error(t);
+                            }
+                        }
+                    })
+            );
+
+        } catch (Throwable t) {
+
+            Logger.error(t);
+
+            return null;
+        }
     }
 
     public interface ConversationEnterListener {
+
         void onConversationEnter(Long threadKey);
     }
 }
