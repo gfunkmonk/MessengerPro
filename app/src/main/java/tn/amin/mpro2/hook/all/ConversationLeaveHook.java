32ace139cfab30c9caf93aeac8ccc98dcf97a46e
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-05-23 23:42:05 +0300

    Update ConversationLeaveHook.java

--- app/src/main/java/tn/amin/mpro2/hook/all/ConversationLeaveHook.java
+++ app/src/main/java/tn/amin/mpro2/hook/all/ConversationLeaveHook.java
@@ -3,6 +3,8 @@ package tn.amin.mpro2.hook.all;
 import java.util.Set;
 
 import de.robv.android.xposed.XC_MethodHook;
+
+import tn.amin.mpro2.debug.Logger;
 import tn.amin.mpro2.hook.BaseHook;
 import tn.amin.mpro2.hook.HookId;
 import tn.amin.mpro2.hook.HookTime;
@@ -11,6 +13,7 @@ import tn.amin.mpro2.hook.unobfuscation.OrcaUnobfuscator;
 import tn.amin.mpro2.orca.OrcaGateway;
 
 public class ConversationLeaveHook extends BaseHook {
+
     @Override
     public HookId getId() {
         return HookId.CONVERSATION_LEAVE;
@@ -22,17 +25,76 @@ public class ConversationLeaveHook extends BaseHook {
     }
 
     @Override
-    protected Set<XC_MethodHook.Unhook> injectInternal(OrcaGateway gateway) {
-        return OrcaHookHelper.hookFeature(gateway.unobfuscator.getAPICode(OrcaUnobfuscator.API_CONVERSATION_LEAVE),
-                "V", "Orca", gateway.classLoader, wrap(new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        notifyListeners((listener) -> ((ConversationLeaveListener) listener).onConversationLeave());
-                    }
-                }));
+    protected Set<XC_MethodHook.Unhook> injectInternal(
+            OrcaGateway gateway
+    ) {
+
+        try {
+
+            Integer apiCode =
+                    gateway.unobfuscator.getAPICode(
+                            OrcaUnobfuscator.API_CONVERSATION_LEAVE
+                    );
+
+            if (apiCode == null) {
+
+                Logger.error(
+                        "Conversation leave API code is null"
+                );
+
+                return null;
+            }
+
+            return OrcaHookHelper.hookFeature(
+
+                    apiCode,
+
+                    "V",
+
+                    "Orca",
+
+                    gateway.classLoader,
+
+                    wrap(new XC_MethodHook() {
+
+                        @Override
+                        protected void beforeHookedMethod(
+                                MethodHookParam param
+                        ) {
+
+                            try {
+
+                                notifyListeners(listener -> {
+
+                                    try {
+
+                                        ((ConversationLeaveListener) listener)
+                                                .onConversationLeave();
+
+                                    } catch (Throwable listenerError) {
+
+                                        Logger.error(listenerError);
+                                    }
+                                });
+
+                            } catch (Throwable t) {
+
+                                Logger.error(t);
+                            }
+                        }
+                    })
+            );
+
+        } catch (Throwable t) {
+
+            Logger.error(t);
+
+            return null;
+        }
     }
 
     public interface ConversationLeaveListener {
+
         void onConversationLeave();
     }
 }
